951bb4e656c83722ea93ce0c26884978fca3199a
KayceeJayy <justinkaycee@gmail.com>
2021-08-31 20:25:53 +0100

    9 update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,7 +27,7 @@ void times_table(void)
 			else if (res < 10 && j != 0)
 			{
 				_putchar(' ');
-				_putchar((res % 10) + '0')
+				_putchar((res % 10) + '0');
 			}
 			else
 				_putchar((res % 10) + '0');
