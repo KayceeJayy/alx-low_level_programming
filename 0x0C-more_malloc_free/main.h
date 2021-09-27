7a16f3542d17166323c431bc67b78174e8c718f6
KayceeJayy <justinkaycee@gmail.com>
2021-09-27 10:36:30 +0100

    101 main update

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,6 +8,5 @@ int *array_range(int min, int max);
 int _putchar(char);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _puts(const char *str);
-int main(int argc, char **argv);
 
 #endif
