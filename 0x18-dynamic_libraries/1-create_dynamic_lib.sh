0db2b9b58f86f35452ba9422d7a7c28cc09c8dd3
KayceeJayy <justinkaycee@gmail.com>
2021-12-01 16:18:44 +0100

    shell update

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -fpic -c *.c
-gcc -shared -o liball.so *.o
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -fPIC -c *.c
+gcc *.o -shared -o liball.so
 chmod 644 liball.so
 export LD_LIBRARY_PATH=".:$LD_LIBRARY_PATH"
